18cd39e9026ef2d170fbc5f870e641ffbd536722
Nach0Zar <50642766+Nach0Zar@users.noreply.github.com>
2022-08-27 12:33:46 -0300

    fixed Syntax

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
         <div id="containerTitulos">
                   <h1>Zona Compra E-Commerce</h1>
                   <h2>E-Commerce dedicado a sudamerica</h2>
-                  <label for="busqueda">
+                  <label htmlFor="busqueda">
                       <input type="search" id="busqueda" placeholder="Buscar elementos en el catalogo"/>
-                      <button type="button" class="btn btn-outline-dark">Buscar</button>
+                      <button type="button" className="btn btn-outline-dark">Buscar</button>
                   </label>
               </div>
           <hr/>
--- src/components/itemListContainer.jsx
+++ src/components/itemListContainer.jsx
@@ -7,22 +7,22 @@ const ItemListContainer = () => {
             <hr/>
             <h2>Recomendados</h2>
             <div id="recomendados">
-                    <div class="itemDiv itemRecomendado">
+                    <div className="itemDiv itemRecomendado">
                             <img src="https://www.lg.com/us/images/tvs/50pk540/gallery/large01.jpg" alt=""/>
                             <hr/>
                             <span>$25000</span>
                     </div>
-                    <div class="itemDiv itemRecomendado">
+                    <div className="itemDiv itemRecomendado">
                             <img src="https://cdn.static.escuelamakeup.com/imagenes/de-que-estan-hechos-los-cosmeticos_905x603.jpg" alt=""/>
                             <hr/>
                             <span>$45000</span>
                     </div>
-                    <div class="itemDiv itemRecomendado">
+                    <div className="itemDiv itemRecomendado">
                             <img src="https://jumboargentina.vtexassets.com/arquivos/ids/614663/Aceite-De-Girasol-Cocinero-900-Ml-1-32670.jpg?v=637409202878630000" alt=""/>
                             <hr/>
                             <span>$5000</span>
                     </div>
-                    <div class="itemDiv itemRecomendado">
+                    <div className="itemDiv itemRecomendado">
                             <img src="https://http2.mlstatic.com/D_NQ_NP_968821-MLA31587799913_072019-O.jpg" alt=""/>
                             <hr/>
                             <span>$2000</span>
